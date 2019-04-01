a5239d0b55b4f86fc3e06bfdd0a290bdc0e8c8fd
levent yıldız <leventyildiz@leventyildiz.local>
2019-04-01 23:37:18 +0300

    update Daily report Graph

--- mobile/manager/src/lib/db/sensorData/index.js
+++ mobile/manager/src/lib/db/sensorData/index.js
@@ -20,6 +20,50 @@ export const addSensorData = (deviceId, nodeId, sensorId, sensorCreateDate, data
     });
 }
 
+export const getDailyAverageBySensorType = (deviceId, nodeId, sensorId) => {
+    return new Promise((resolve, reject) => {
+        db.transaction((tx) => {
+
+            let sqlQuery = `SELECT
+                             (T.total / T.count) as average,
+                             T.formattedDate
+                           FROM (
+                                  SELECT
+                                    strftime('%Y-%m-%d', formattedSensorCreateDate) as formattedDate,
+                                    count(*)                                        as count,
+                                    sum(data)                                       as total
+                                  FROM sensorData
+                                  WHERE formattedDate is not null
+                                        AND deviceId = ?
+                                        AND nodeId = ?
+                                        AND sensorId = ?
+                                  GROUP BY formattedDate
+                                ) as T
+                           ORDER BY T.formattedDate
+                             ASC`
+
+            tx.executeSql(sqlQuery, [deviceId, nodeId, sensorId], (tx, results) => {
+
+                let len = results.rows.length;
+
+                if (len > 0) {
+                    let listRes = []
+
+                    for (let i = 0; i < len; i++) {
+                        listRes.push(results.rows.item(i));
+                    }
+
+                    resolve(listRes);
+                }
+
+                resolve(null);
+
+            });
+        });
+    });
+}
+
+
 export const getWeeklyAverageBySensorType = (deviceId, nodeId, sensorId) => {
     return new Promise((resolve, reject) => {
         db.transaction((tx) => {
--- mobile/manager/src/screen/Reports/Daily/Data.js
+++ mobile/manager/src/screen/Reports/Daily/Data.js
@@ -2,18 +2,16 @@ import React, {Component} from 'react';
 import {View} from 'react-native'
 import {Container, Content} from 'native-base';
 
-import {AreaChart, BarChart, Grid, LineChart, PieChart} from 'react-native-svg-charts'
-import * as shape from 'd3-shape'
-
 import {CtxConsumer} from 'MgrBoot/Container';
 import SideBarNav from 'MgrComponent/SideBarNav';
 import * as sensor from 'MgrLib/sensor';
 import * as moment from "MgrLib/moment";
 import {lang} from 'MgrLocale';
+import Graph from 'MgrComponent/Graph';
+import * as Color from 'MgrUtil/Color';
+import * as sensorDataDb from 'MgrLib/db/sensorData';
 
-
-const fill = 'rgb(134, 65, 244)'
-
+const defaultGraphData = {labels: [], datasets: [{data: []}]};
 
 export default class DailyGraphData extends Component {
     render() {
@@ -32,7 +30,8 @@ export class DailyGraphDataContext extends Component {
         super(props)
         this.state = {
             rerender: false,
-            chartsData: []
+            graphData: defaultGraphData,
+            pieData: [],
         }
     }
 
@@ -43,103 +42,41 @@ export class DailyGraphDataContext extends Component {
 
         const endDate = moment.currTime();
         const startDate = moment.lastYear();
-        let chartsData = [];
+        let graphData = {
+            labels: [],
+            datasets: [{data: []}]
+        };
+        let pieData = [];
 
         const d = await sensor.getSensorHistory(context.token, context.deviceId, context.node.nodeId, context.sensor.id, startDate, endDate);
 
-        d.map((v, k) => {
-            const sensorData = JSON.parse(v.data);
-            chartsData.push(parseInt(sensorData[0]));
+        await sensorDataDb.deleteAllSensorData();
+
+        await d.map((v, k) => {
+            const formattedSensorCreateDate = moment.getHumanDateAndTime(v.createDate);
+            let sensorData = JSON.parse(v.data);
+            sensorData = parseInt(sensorData[0]);
+            sensorDataDb.addSensorData(v.deviceId, v.nodeId, v.sensorId, v.createDate, sensorData, formattedSensorCreateDate);
         });
 
-        self.setState({chartsData});
+        const dbData = await  sensorDataDb.getDailyAverageBySensorType(context.deviceId, context.node.nodeId, context.sensor.id);
+
+        dbData.map((v, k) => {
+
+            pieData.push({
+                name: v.formattedDate,
+                population: v.average,
+                color: Color.getRandomColor(),
+                legendFontColor: '#7F7F7F',
+                legendFontSize: 15
+            })
+
+            graphData["labels"].push(v.formattedDate);
+            graphData.datasets[0].data.push(v.average);
+        });
+
+        self.setState({graphData, pieData});
         context.hideLoading();
-
-    }
-
-    loadBarChart = () => {
-
-        if (!this.state.chartsData || this.state.chartsData.length < 1) {
-            return null;
-        }
-
-        return (
-            <BarChart
-                style={{height: 200}}
-                data={this.state.chartsData}
-                svg={{fill}}
-                contentInset={{top: 30, bottom: 30}}
-            >
-                <Grid/>
-            </BarChart>
-        );
-    }
-
-    loadLineChart = () => {
-
-        if (!this.state.chartsData || this.state.chartsData.length < 1) {
-            return null;
-        }
-
-        return (
-            <View style={{height: 200, padding: 20}}>
-                <LineChart
-                    style={{flex: 1}}
-                    data={this.state.chartsData || []}
-                    gridMin={0}
-                    contentInset={{top: 10, bottom: 10}}
-                    svg={{stroke: 'rgb(134, 65, 244)'}}
-                >
-                    <Grid/>
-                </LineChart>
-            </View>
-        );
-    }
-
-    loadPieChart = () => {
-
-        const randomColor = () => ('#' + (Math.random() * 0xFFFFFF << 0).toString(16) + '000000').slice(0, 7)
-
-        const pieData = this.state.chartsData
-            .filter(value => value > 0)
-            .map((value, index) => ({
-                value,
-                svg: {
-                    fill: randomColor(),
-                    onPress: () => console.log('press', index),
-                },
-                key: `pie-${index}`,
-            }))
-
-        if (!this.state.chartsData || this.state.chartsData.length < 1) {
-            return null;
-        }
-
-        return (
-            <PieChart
-                style={{height: 200}}
-                data={pieData}
-            />
-        );
-    }
-
-    loadAreaChart = () => {
-
-        if (!this.state.chartsData || this.state.chartsData.length < 1) {
-            return null;
-        }
-
-        return (
-            <AreaChart
-                style={{height: 200}}
-                data={this.state.chartsData || []}
-                contentInset={{top: 30, bottom: 30}}
-                curve={shape.curveNatural}
-                svg={{fill: 'rgba(134, 65, 244, 0.8)'}}
-            >
-                <Grid/>
-            </AreaChart>
-        );
     }
 
     render() {
@@ -153,13 +90,11 @@ export class DailyGraphDataContext extends Component {
                 <SideBarNav pageTitle={lang(locale).getLabel("screen.reports.dailyTitle")}/>
 
                 <Content>
-                    {this.loadBarChart()}
-                    {this.loadLineChart()}
-                    {this.loadPieChart()}
-                    {this.loadAreaChart()}
+                    <Graph pieData={this.state.pieData} graphData={this.state.graphData}/>
                 </Content>
 
             </Container>
         );//return
     }//render
+
 }
