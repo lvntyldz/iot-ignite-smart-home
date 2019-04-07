3d2df6aa6b6d1620730a05b041f20e7b1b3495ca
levent yıldız <leventyildiz@leventyildiz.local>
2019-04-08 00:06:52 +0300

    add sample test code

---
+++ mobile/manager/__tests__/HomeTest.js
@@ -0,0 +1,6 @@
+import 'react-native' import React from 'react';
+import Home from '../src/Home.js';
+
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () = > { renderer.create(<Home />); });
