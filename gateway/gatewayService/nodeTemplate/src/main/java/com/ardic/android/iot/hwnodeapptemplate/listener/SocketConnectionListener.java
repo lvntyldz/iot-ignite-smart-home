4518bc0fbfa6a9285a9f1df833aa0071a1a57ca7
levent yıldız <leventyildiz@leventyildiz.local>
2019-06-01 13:22:01 +0300

    refactor headlessGateway

--- gateway/gatewayService/nodeTemplate/src/main/java/com/ardic/android/iot/hwnodeapptemplate/listener/SocketConnectionListener.java
+++
@@ -1,9 +0,0 @@
-
-package com.ardic.android.iot.hwnodeapptemplate.listener;
-
-public interface SocketConnectionListener {
-
-    public void onDataReceived(String message);
-
-    public void onConnectionLost();
-}
--- gateway/gatewayService/nodeTemplate/src/main/java/com/ardic/android/iot/hwnodeapptemplate/listener/WifiNodeListener.java
+++
@@ -1,9 +0,0 @@
-package com.ardic.android.iot.hwnodeapptemplate.listener;
-
-
-import com.ardic.android.iot.hwnodeapptemplate.object.WifiNode;
-
-public interface WifiNodeListener {
-    void onWifiNodeAdded(WifiNode device);
-    void onWifiNodeAddressChange(WifiNode device);
-}
